809ed07d27785e6ed40a581e4446714242c5af3b
José Chu <jchudb93@gmail.com>
2018-01-04 15:05:29 -0500

    Components

--- udemy/news/src/App.js
+++ udemy/news/src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import list from './list.js'
 
-function isSearched(searchTerm){searchTerm
+function isSearched(searchTerm){
   return function(item){
     return !searchTerm || item.title.includes(searchTerm);
   }
@@ -15,21 +15,11 @@ class App extends Component {
     this.state = {
       list: list
     }
-
     //bind the method*
     this.removeItem = this.removeItem.bind(this);
     this.searchValue = this.searchValue.bind(this);
   }
 
-  // removeItem(id){
-  //   console.log("Remove Item");
-  //   function isNotId(item){
-  //     return item.objectID !== id;
-  //   }
-  //
-  //   const updatedList = this.state.list.filter(isNotId);
-  //   this.setState({ list: updatedList});
-  // }
   removeItem(id){
     //const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(item => item.objectID !== id);
@@ -40,24 +30,74 @@ class App extends Component {
     this.setState({searchTerm: event.target.value})
   }
   render() {
+    const {list, searchTerm} = this.state;
     return (
       <div className="App">
-        <form>
-            <input type="text" onChange={ this.searchValue  }/>
-        </form>
-        {
-         this.state.list.filter( isSearched(this.state.searchTerm)).map((item) =>
-           <div key={item.objectID}>
-              <h1> {item.title}</h1>
-              <h4> {item.num_comments}</h4>
-              <button type="button" onClick = { () => this.removeItem(item.objectID)}>Remove</button>
-              <p>{item.author}</p>
-           </div>
-         )
-       }
+        <Search
+        onChange={ this.searchValue }
+        value = { searchTerm }
+        >
+        Search here
+        </Search>
+
+        <Table
+        list = { list }
+        searchTerm = { searchTerm }
+        removeItem = { this.removeItem }
+        />
+
       </div>
     );
   }
 }
 
+class Search extends Component{
+  render(){
+    const {onChange, value,children} = this.props
+    return(
+      <form>
+          {children}
+          <input
+          type="text"
+          onChange={ onChange }
+          value = { value }
+          />
+      </form>
+    )
+  }
+}
+
+class Table extends Component{
+  render (){
+    const {list, searchTerm, removeItem} = this.props
+    return(
+      <div>
+      {
+       list.filter( isSearched(searchTerm)).map((item) =>
+         <div key={item.objectID}>
+            <h1> {item.title}</h1>
+            <h4> {item.num_comments}</h4>
+            <Button
+            type = "button"
+            onClick = { () => this.removeItem(item.objectID) }>
+            Remove Item
+            </Button>
+            <p>{item.author}</p>
+         </div>
+        )
+      }
+      </div>
+    )
+  }
+}
+
+class Button extends Component{
+  render(){
+    const {type, onClick, children} = this.props;
+    return(
+      <button onClick = { onClick } >{ children }</button>
+    )
+
+  }
+}
 export default App;
