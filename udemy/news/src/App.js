0a22a51fd556f36fece7b0daa01a269e7fcf83a5
José Chu <jchudb93@gmail.com>
2018-01-04 14:34:55 -0500

    ES 6

--- udemy/news/src/App.js
+++ udemy/news/src/App.js
@@ -2,48 +2,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import list from './list.js'
 
-const list = [
-  {
-  title: 'Tell mee the truth without words',
-  url:'http://kaloraat.com',
-  author:'Ryan',
-  num_comments: 100,
-  points: 50,
-  objectID: 1
-},
-
-{
-  title: 'Oh no',
-  url:'http://kaloraat.com',
-  author:'Ninja',
-  num_comments: 100,
-  points: 50,
-  objectID: 2
-},
-
-{
-  title: 'Oh no',
-  url:'http://kaloraat.com',
-  author:'Clint',
-  num_comments: 100,
-  points: 50,
-  objectID: 3
+function isSearched(searchTerm){searchTerm
+  return function(item){
+    return !searchTerm || item.title.includes(searchTerm);
+  }
 }
-]
-
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list: list
+    }
+
+    //bind the method*
+    this.removeItem = this.removeItem.bind(this);
+    this.searchValue = this.searchValue.bind(this);
+  }
+
+  // removeItem(id){
+  //   console.log("Remove Item");
+  //   function isNotId(item){
+  //     return item.objectID !== id;
+  //   }
+  //
+  //   const updatedList = this.state.list.filter(isNotId);
+  //   this.setState({ list: updatedList});
+  // }
+  removeItem(id){
+    //const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList});
+  }
+
+  searchValue(event){
+    this.setState({searchTerm: event.target.value})
+  }
   render() {
     return (
-      <div className="App">{
-         list.map(function(item){
-           return(
+      <div className="App">
+        <form>
+            <input type="text" onChange={ this.searchValue  }/>
+        </form>
+        {
+         this.state.list.filter( isSearched(this.state.searchTerm)).map((item) =>
            <div key={item.objectID}>
               <h1> {item.title}</h1>
+              <h4> {item.num_comments}</h4>
+              <button type="button" onClick = { () => this.removeItem(item.objectID)}>Remove</button>
               <p>{item.author}</p>
-           </div>)
-         })
+           </div>
+         )
        }
       </div>
     );
---
+++ udemy/news/src/list.js
@@ -0,0 +1,30 @@
+
+const list = [
+  {
+  title: 'Tell mee the truth without words',
+  url:'http://kaloraat.com',
+  author:'Ryan',
+  num_comments: 100,
+  points: 50,
+  objectID: 1
+},
+
+{
+  title: 'Oh no',
+  url:'http://kaloraat.com',
+  author:'Ninja',
+  num_comments: 20,
+  points: 3,
+  objectID: 2
+},
+
+{
+  title: 'Iwojima',
+  url:'http://kaloraat.com',
+  author:'Clint',
+  num_comments: 100,
+  points: 20,
+  objectID: 3
+}
+]
+export default list;
