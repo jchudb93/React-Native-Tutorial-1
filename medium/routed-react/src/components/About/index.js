ca03687ee2456a1ddb763c23ca147edfbcf392ff
José Chu <jchudb93@gmail.com>
2018-01-05 19:52:09 -0500

    express js

---
+++ medium/routed-react/src/components/About/index.js
@@ -0,0 +1,17 @@
+import React, { PropTypes, Component} from 'react';
+import classnames from 'classnames';
+
+import './style.css';
+
+export default class About extends Component {
+  render(){
+    const { className, ...props } = this.props;
+    return (
+      <div className={classnames('About', className)} {...props}>
+      <h1>
+        About
+      </h1>
+    </div>
+    );
+  }
+}
--- medium/routed-react/src/components/App/index.js
+++ medium/routed-react/src/components/App/index.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const {className, ...props} = this.props;
     return (
-      <div className="App">
+      <div className={classnames('App',className)} {...props}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
