22488456206ee09225e908d9c194fd34afd5f48e
jose chu <jchudb93@gmail.com>
2018-03-25 21:03:34 -0500

    prisma

--- GraphQL/ReactGraphQL/hackernews-react-apollo/server/src/index.js
+++ GraphQL/ReactGraphQL/hackernews-react-apollo/server/src/index.js
@@ -19,7 +19,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://us1.prisma.sh/public-jellyeagle-702/ha',
+      endpoint: 'https://us1.prisma.sh/public-jellyeagle-702/hackernews-graphql-js/dev',
       secret: 'mysecret123',
       debug: true
     }),
