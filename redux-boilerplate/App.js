7ad5d22c20c29a9010e6bdfd88cda949ea994032
José Chu <jchudb93@gmail.com>
2018-04-16 00:09:16 -0500

    adding to app

--- redux-boilerplate/App.js
+++ redux-boilerplate/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import { StyleSheet, Text, View } from 'react-native';
+
+import store from './app/store'
+import Home from './app/components/home'
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Provider store={store}>
+          <Home/>
+        </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
